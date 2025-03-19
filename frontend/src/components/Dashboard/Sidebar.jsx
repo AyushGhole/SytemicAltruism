15e740dc79d0e3d0fee1de5191218ddff1872c57
Ayush Ghole <ayushghole@gmail.com>
2025-03-19 12:06:38 +0530

    Update Sidebar.jsx

--- frontend/src/components/Dashboard/Sidebar.jsx
+++ frontend/src/components/Dashboard/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import axios from "axios";
 
-const socket = io("https://sytemicaltruism-backend.onrender.com");
+const socket = io("https://sytemic-altruism-backend.vercel.app/");
 
 const Sidebar = ({ setSelectedChat }) => {
   const [users, setUsers] = useState([]);
